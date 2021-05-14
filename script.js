593573c4149f1bbc93e96fae06f616d8ba3c2aeb
Vitor-Rafael-Ferreira <80597119+Vitor-Rafael-Ferreira@users.noreply.github.com>
2021-05-14 19:30:47 -0300

    adicionando o html, css e JS

---
+++ script.js
@@ -0,0 +1,7 @@
+const anoatual = "2021"
+
+const nome = prompt("Digite o seu nome aqui!") 
+
+const anonascimento  =  parseInt(prompt("Digite o ano em que você nasceu aqui!") )
+
+alert (`Ah, então o seu nome é ${nome} e você tem ${anoatual - anonascimento} anos, que legal! `)
